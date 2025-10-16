e887ece641273abe237069df8728329c31ae9f95
Erwin Suyatno <erwinbonchu61@gmail.com>
2025-10-16 23:14:43 +0700

    chore(jwt): Simplify JWT signing function

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
 
 export function signJwt(payload: object, expiresIn: string | number = process.env.JWT_EXPIRES_IN || '1d') {
-  return jwt.sign(payload as any, process.env.JWT_SECRET as string, { expiresIn });
+  return jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn } as jwt.SignOptions);
 }
